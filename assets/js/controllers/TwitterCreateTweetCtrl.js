2b5231c53e5537dd5425f11db0eeed8443ff7420
fisch0920 <fisch0920@gmail.com>
2014-12-25 11:20:50 -0500

    .

--- assets/js/controllers/TwitterCreateTweetCtrl.js
+++ assets/js/controllers/TwitterCreateTweetCtrl.js
@@ -2,9 +2,24 @@
 angular.module('fakepost').controller('TwitterCreateTweetCtrl', function (
   $scope, $stateParams, $http, flash)
 {
-  $scope.avatars = []
+  $scope.tweet = {
+    user: {
+      username: "fisch0920",
+      fullname: "Travis Fischer",
+      avatar: "https://pbs.twimg.com/profile_images/547714403684986880/apxt8hY7_bigger.png",
+      verified: true
+    },
+    stats: {
+      retweets: 5,
+      favorites: 7,
+      favoritedBy: []
+    },
+    status: "This just in: Russell Wilson is good.",
+    timestamp: "5:03 AM - 25 Dec 2014"
+  }
+
   for (var i = 0; i < 10; ++i) {
-    $scope.avatars.push(faker.image.avatar())
+    $scope.tweet.stats.favoritedBy.push(faker.image.avatar())
   }
 });
 
