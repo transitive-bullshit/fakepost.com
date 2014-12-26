3924f240c1430089c079c7c526d7d49600ea38a1
fisch0920 <fisch0920@gmail.com>
2014-12-25 19:02:14 -0500

    .

--- assets/js/controllers/TwitterCreateTweetCtrl.js
+++ assets/js/controllers/TwitterCreateTweetCtrl.js
@@ -14,12 +14,37 @@ angular.module('fakepost').controller('TwitterCreateTweetCtrl', function (
       favorites: 7,
       favoritedBy: []
     },
-    status: "This just in: Russell Wilson is good.",
+    status: "Create your own fake twitter post now! Check it out at www.fakepost.com",
     timestamp: "5:03 AM - 25 Dec 2014"
   }
 
   for (var i = 0; i < 10; ++i) {
     $scope.tweet.stats.favoritedBy.push(faker.image.avatar())
+  }
+
+  $scope.refreshAvatar = function (index) {
+    $scope.safeApply(function () {
+      $scope.tweet.stats.favoritedBy[index] = faker.image.avatar()
+    })
+  }
+
+  $scope.editing = {
+    username: false,
+    status: false,
+    retweets: false,
+    favorites: false,
+    timestamp: false,
+  }
+
+  $scope.toggleEditing = function (key) {
+    $scope.safeApply(function () {
+      for (var k in $scope.editing) {
+        $scope.editing[k] = false
+      }
+
+      $scope.editing[key] = !$scope.editing[key]
+      console.log(key, $scope.editing[key])
+    })
   }
 });
 
